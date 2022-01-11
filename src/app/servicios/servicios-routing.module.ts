8cbb348fa51a3f60f92622072064d1c863a6af73
Sergio Cardoso <sergio.cardhdez97@gmail.com>
2022-01-11 13:02:36 -0600

    Creacion de todos los componentes modularizados

---
+++ src/app/servicios/servicios-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ServiciosComponent } from './components/servicios.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: ServiciosComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ServiciosRoutingModule { }
